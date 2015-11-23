92f0b559404bbfde2327079d9170d3609525348a
Oscar <sistemas2@biccofarms.com>
2015-11-23 11:15:32 -0500

    Se crea formulario con servicios en angularjs

---
+++ module1/lab19/js/app.js
@@ -0,0 +1,15 @@
+angular.module('lab19', ['ngRoute', 'lab19Controller', 'ContactPeopleService'])
+
+.config(function($routeProvider){
+
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'views/otherwise.html',
+		controller: 'ContactPeopleController'
+	})
+
+	.otherwise({
+		redirectTo: '/'
+	});
+});
