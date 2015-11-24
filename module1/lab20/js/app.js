6a04dcb29f8a24fbeeee02ebaed1d83eab95a242
Oscar <sistemas2@biccofarms.com>
2015-11-24 07:57:14 -0500

    Se realiza modificación laboratorio 20 en tema css y tabla de datos

--- module1/lab20/js/app.js
+++ module1/lab20/js/app.js
@@ -2,18 +2,18 @@ angular.module('lab20', ['ngRoute', 'lab20Controller', 'ContactPeopleService'])
 
 .config(function($routeProvider){
 
-	$routeProvider
+    $routeProvider
 
-	.when('/listRed', {
-		templateUrl: 'views/list_red.html',
-		controller: 'ContactOdd'
-	})
-	.when('/listGreen', {
-		templateUrl: 'views/list_green.html',
-		controller: 'ContactEven'
-	})
+    .when('/listRed', {
+            templateUrl: 'views/list.html',
+            controller: 'ContactOdd'
+    })
+    .when('/listGreen', {
+            templateUrl: 'views/list.html',
+            controller: 'ContactEven'
+    })
 
-	.otherwise({
-		redirectTo: '/'
-	});
+    .otherwise({
+            redirectTo: '/'
+    });
 });
--- module1/lab20/js/controllers.js
+++ module1/lab20/js/controllers.js
@@ -1,29 +1,12 @@
 angular.module('lab20Controller', ['ContactPeopleService'])
 
 .controller('ContactOdd', function($scope, Data){
-    var list = Data.list();
-    var new_list = [];
     
-    for(var i in list){
-        if(i%2){
-            new_list.push(list[i]);
-        }
-    }
-    
-    $scope.list = new_list;
+    $scope.list = Data.list();
+    $scope.special_class = 'odd';
 })
 .controller('ContactEven', function($scope, Data){
 
-    var list = Data.list();
-    var new_list = [];
-    
-    for(var i in list){
-        if(i%2){
-            
-        }else{
-            new_list.push(list[i]);
-        }
-    }
-    
-    $scope.list = new_list;
+    $scope.list = Data.list();
+    $scope.special_class = 'even';
 });
